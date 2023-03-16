fc898b78475b48755262960ab7d89705f82f035e
Saurabh <125241825+SaurabhBelsare13@users.noreply.github.com>
2023-03-16 07:06:12 +0000

    create page for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_data_from_db
+from database import load_data_from_db, load_job_from_db
 # from database import load_data_from_jobs
 
 app = Flask (__name__)  
@@ -34,13 +34,24 @@ app = Flask (__name__)
 
 @app.route("/")
 def hello_world():
-  jobs_list = load_data_from_db()
-  return render_template('home.html',jobs=jobs_list)
+  jobs = load_data_from_db()
+  return render_template('home.html',jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():     
-  jobs_list = load_data_from_db()
-  return jsonify(jobs_list)
+  jobs = load_data_from_db()
+  return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+
+  if not job:
+    return "Not found",404
+  else:
+    return render_template('jobpage.html', job=job)
+
+  
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -23,4 +23,16 @@ def load_data_from_db():
 
     return jobs
 
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text('SELECT * FROM jobs where id = :val'), 
+           val=id)
+    
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
+
+
 
